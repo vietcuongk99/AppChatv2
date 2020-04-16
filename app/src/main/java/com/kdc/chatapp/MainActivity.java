72c6b9b79aada1a55682a3b8e94d631c08e7b97b
DaoDucKhiem <daoduckhiem1104@gmail.com>
2020-04-16 17:27:58 +0700

    add setting image and find friends

--- app/src/main/java/com/kdc/chatapp/MainActivity.java
+++ app/src/main/java/com/kdc/chatapp/MainActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -116,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (item.getItemId() == R.id.main_find_friends_option) {
-
+            SenUserToFindFriendsActivity();
         }
 
         if (item.getItemId() == R.id.main_create_group_option) {
@@ -172,16 +171,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void SenUserToLoginActivity() {
-        Intent LoginInten = new Intent(MainActivity.this, LoginActivity.class);;
-        LoginInten.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(LoginInten);
+        Intent LoginIntent = new Intent(MainActivity.this, LoginActivity.class);;
+        LoginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(LoginIntent);
         finish();
     }
 
     private void SenUserToSettingsActivity() {
-        Intent settingsInten = new Intent(MainActivity.this, SettingsActivity.class);;
-        settingsInten.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(settingsInten);
+        Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);;
+        settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(settingsIntent);
+        finish();
+    }
+
+    private void SenUserToFindFriendsActivity() {
+        Intent findFriendsIntent = new Intent(MainActivity.this, FindFriendsActivity.class);;
+        findFriendsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(findFriendsIntent);
         finish();
     }
 }
