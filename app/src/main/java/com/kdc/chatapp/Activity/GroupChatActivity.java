603960383033a8ace3a09b62410c956d640c148f
duong17020661 <duong.coi.bs.1104@gmail.com>
2020-06-16 08:56:22 +0700

    update location sharing 2

--- app/src/main/java/com/kdc/chatapp/Activity/GroupChatActivity.java
+++ app/src/main/java/com/kdc/chatapp/Activity/GroupChatActivity.java
@@ -495,16 +495,6 @@ public class GroupChatActivity extends AppCompatActivity {
 
                 }
             };
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000000000, 0, locationListener);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000000000, 0, locationListener);
         }
